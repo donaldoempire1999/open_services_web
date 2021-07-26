9bccaaafef73927003976d38343fb9fad355f5a6
donaldoempire1999 <donaldfotso1999@gmail.com>
2021-07-26 01:59:00 +0100

    Update readme.txt and structure

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/index', { title: 'Express' });
 });
 
 
 router.get('/service', function(req, res, next) {
-  res.render('service', { title: 'Express' });
+  res.render('pages/service', { title: 'Express' });
 });
 
 
 router.get('/profile', function(req, res, next) {
-  res.render('profile', { title: 'Express' });
+  res.render('pages/profile', { title: 'Express' });
 });
 
 
 router.get('/registration', function(req, res, next) {
-  res.render('registration', { title: 'Express' });
+  res.render('pages/registration', { title: 'Express' });
 });
 
 router.get('/team', function(req, res, next) {
-  res.render('team', { title: 'Express' });
+  res.render('pages/team', { title: 'Express' });
 });
 
 
 router.get('/about', function(req, res, next) {
-  res.render('about');
+  res.render('pages/about');
 });
 
 module.exports = router;
