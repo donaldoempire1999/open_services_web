b3e985e40dbaaf0e9d4342e2185bdb46276dcfea
exceldev-237 <excelsime10@gmail.com>
2021-07-26 16:23:56 +0100

    repusch

---
+++ public/js/app/registration.js
@@ -0,0 +1,21 @@
+import {User} from "./classes/user.js";
+
+$(function () {
+
+    //registration
+    $("#Form_account").on('submit', async function (e) {
+       e.preventDefault();
+       alert(this);
+       let formdata = new FormData(e.target);
+       formdata = Object.fromEntries(formdata.entries());
+       formdata = JSON.stringify(formdata);
+       console.log(formdata);
+       let response = await access("/users/signup", 'POST', formdata);
+       let data = await response.json();
+       console.log(response);
+       console.log(data);
+ 
+    });
+ })
+ 
+ 
